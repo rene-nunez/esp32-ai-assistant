2c5c512f05c953fbf8aaa1b4ef08652e5f791b97
René Núñez <221588492+rene-nunez@users.noreply.github.com>
2026-04-27 22:44:24 -0600

    feat: add IR sensor as trigger for audio streaming

--- src/main.cpp
+++ src/main.cpp
@@ -5,28 +5,31 @@
 
 const char* ssid = "";
 const char* password = "";
-const char* websockets_connection_string = "ws://192.168.0.5:8765";
+const char* websockets_connection_string = "ws://192.168.0.5:8765"; // IP del dispositivo donde corre Python
 
 using namespace websockets;
 WebsocketsClient client;
 
-// Pines (los mismos que ya te funcionaron)
+// Configuración de pines
 #define I2S_WS 15
 #define I2S_SD 32
 #define I2S_SCK 14
 #define I2S_PORT I2S_NUM_0
+#define PIN_IR 13
 
 void setup() {
     Serial.begin(115200);
+
+    pinMode(PIN_IR, INPUT);
+
     WiFi.begin(ssid, password);
-    
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
     }
     Serial.println("\nConectado a WiFi");
 
-    // Configuración I2S (la que ya probamos)
+    // Configuración I2S para el micrófono MH-ET LIVE I2S MEMS
     i2s_config_t i2s_config = {
         .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
         .sample_rate = 16000,
@@ -35,7 +38,7 @@ void setup() {
         .communication_format = I2S_COMM_FORMAT_STAND_I2S,
         .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
         .dma_buf_count = 8,
-        .dma_buf_len = 512, // Buffer más grande para streaming
+        .dma_buf_len = 512,
         .use_apll = false
     };
 
@@ -54,9 +57,9 @@ void setup() {
     bool connected = client.connect(websockets_connection_string);
     
     if(connected) {
-        Serial.println("¡Conectado exitosamente!");
+        Serial.println("¡Conectado!");
     } else {
-        Serial.println("Fallo inicial, reintentando...");
+        Serial.println("Fallo al intentar conectar, reintentando...");
         while(!client.connect(websockets_connection_string)) {
             delay(1000);
             Serial.print(".");
@@ -66,12 +69,24 @@ void setup() {
 }
 
 void loop() {
-    int16_t samples[512];
-    size_t bytes_read;
-    
-    i2s_read(I2S_PORT, &samples, sizeof(samples), &bytes_read, portMAX_DELAY);
-    
-    if (bytes_read > 0) {
-        client.sendBinary((const char*)samples, bytes_read);
+    // Leemos el sensor IR
+    bool manoDetectada = (digitalRead(PIN_IR) == LOW);
+
+    if (manoDetectada) {
+        int16_t samples[512];
+        size_t bytes_read;
+        
+        // Leemos audio del micrófono
+        i2s_read(I2S_PORT, &samples, sizeof(samples), &bytes_read, portMAX_DELAY);
+        
+        if (bytes_read > 0) {
+            client.sendBinary((const char*)samples, bytes_read); // Solo enviamos los bytes si el sensor IR está activo
+        }
+    } else {
+        // Si el sensor IR no está activo, no enviamos nada
+        delay(10); 
     }
+
+    // Mantenemos en loop la conexión Websockets
+    client.poll();
 }
