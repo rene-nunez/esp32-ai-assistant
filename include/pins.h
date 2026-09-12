86d40f3dca532a3769f4150cc6f5cc3d0ab76fb7
René Núñez <221588492+rene-nunez@users.noreply.github.com>
2026-09-12 12:10:52 -0600

    chore: organize include files in config and pins

---
+++ include/pins.h
@@ -0,0 +1,18 @@
+#ifndef PINS_H
+#define PINS_H
+
+// MH-ET LIVE I2S MEMS Microphone
+#define MIC_SCK  32
+#define MIC_WS   33
+#define MIC_SD   35
+
+// Amplifier MAX98357A I2S DAC
+#define AMP_BCLK 26
+#define AMP_LRC  27
+#define AMP_DOUT 25
+
+// Button and LED
+#define PIN_BTN  17
+#define PIN_LED  15
+
+#endif
